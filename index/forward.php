af8b3e0cc5d1b797f0fd1916fd32c7887bdce8be
Jiri Zamecnik <jerry.zamecnik@gmail.com>
2019-09-24 17:56:11 +0200

    Experimental forwarding

---
+++ index/forward.php
@@ -0,0 +1,9 @@
+ <?php
+ 
+//pull form fields into php variables
+$next = $_POST['next'];
+
+echo '<p style="text-align:center; font-family: Lucida, Console, monospace; font-size: medium;">You can take a break now.</p><p><a href="'.$next.'">Click here to continue</a></p>;
+	//echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+
+?>
