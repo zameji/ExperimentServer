95ed19f22ddd63e8c19fc59f30ea0aad1eaa532e
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2019-11-21 20:19:58 +0100

    update Author Recog. Task

--- vocab/createhtml2.py
+++ vocab/createhtml2.py
@@ -72,32 +72,32 @@ authors = [
         "Herman Wouk"
 ]
 
-table = "<table><p><b>Vocabulary</b></p>"
-table += "<p style='text-align:left'>In the test below, you are given a word in capital letters. Select the word that means the same thing, or most nearly the same thing, as the capitalized word.</p>"
-table += "<p style='text-align:left'>If you don't know, guess.</p>"
-
-q_number = 1
-
-for list in words:
-    answers = []
-    table += "<tr><td>"+ str(q_number) + ". " + list[0] + ":</td><td>"
-    correct = "<input name='vocab_" + str(q_number) +"' type='radio' value='1' class='obligatory' required='required' id='vocab_" + str(q_number) + "_1' />"
-    correct += "<label for='vocab_" + str(q_number) + "_1'>" + list[1] + "</label><br />"
-    answers.append(correct)
-    incorrect_1 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
-    incorrect_1 += "<label for='vocab_" + str(q_number) + "_0'>" + list[2] + "</label><br />"
-    answers.append(incorrect_1)
-    incorrect_2 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
-    incorrect_2 += "<label for='vocab_" + str(q_number) + "_0'>" + list[3] + "</label><br />"
-    answers.append(incorrect_2)
-    incorrect_3 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
-    incorrect_3 += "<label for='vocab_" + str(q_number) + "_0'>" + list[4] + "</label><br />"
-    answers.append(incorrect_3)
-    random.shuffle(answers)
-    for item in answers:
-        table += item
-    table += "<br />"
-    q_number +=1
+# table = "<table><p><b>Vocabulary</b></p>"
+# table += "<p style='text-align:left'>In the test below, you are given a word in capital letters. Select the word that means the same thing, or most nearly the same thing, as the capitalized word.</p>"
+# table += "<p style='text-align:left'>If you don't know, guess.</p>"
+#
+# q_number = 1
+#
+# for list in words:
+#     answers = []
+#     table += "<tr><td>"+ str(q_number) + ". " + list[0] + ":</td><td>"
+#     correct = "<input name='vocab_" + str(q_number) +"' type='radio' value='1' class='obligatory' required='required' id='vocab_" + str(q_number) + "_1' />"
+#     correct += "<label for='vocab_" + str(q_number) + "_1'>" + list[1] + "</label><br />"
+#     answers.append(correct)
+#     incorrect_1 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+#     incorrect_1 += "<label for='vocab_" + str(q_number) + "_0'>" + list[2] + "</label><br />"
+#     answers.append(incorrect_1)
+#     incorrect_2 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+#     incorrect_2 += "<label for='vocab_" + str(q_number) + "_0'>" + list[3] + "</label><br />"
+#     answers.append(incorrect_2)
+#     incorrect_3 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+#     incorrect_3 += "<label for='vocab_" + str(q_number) + "_0'>" + list[4] + "</label><br />"
+#     answers.append(incorrect_3)
+#     random.shuffle(answers)
+#     for item in answers:
+#         table += item
+#     table += "<br />"
+#     q_number +=1
 
 table += "</table>"
 
