812554e282f808f3e86bbe412b532f1fa56e85f4
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2019-11-22 17:58:22 +0100

    Update ART, add Collocation task

--- vocab/createhtml2.py
+++ vocab/createhtml2.py
@@ -2,102 +2,167 @@ import random
 import sys
 
 ## Author recognition task
-#source: Acheson et al. 2008, Hintz & Brysbaert 2019
+#source: Acheson et al. 2008, Hintz & Brysbaert 2019, Mar et al 2006 (updated version with different genres, what we use here)
 
 authors = [
-        "Maya Angelou",
-        "Isaac Asimov",
-        "Jean M. Auel",
-        "James Clavell",
-        "Jackie Collins",
-        "Isabel Allende",
-        "Margaret Atwood",
-        "Ann Beattie",
-        "Samuel Beckett",
-        "Saul Bellow",
-        "T. C. Boyle",
-        "Ray Bradbury",
-        "Willa Cather",
-        "Raymond Chandler",
-        "Tom Clancy",
-        "Clive Cussler",
-        "Nelson Demille",
-        "Umberto Eco",
-        "T. S. Elliot",
-        "Ralph Ellison",
-        "Nora Ephron",
-        "William Faulkner",
-        "Dick Francis",
-        "Stephen King",
-        "Judith Krantz",
-        "Robert Ludlum",
-        "James Michener",
-        "F. Scott Fitzgerald",
-        "Sue Grafton",
-        "John Grisham",
-        "Ernest Hemmingway",
-        "Brian Herbert",
-        "Tony Hillerman",
-        "John Irving",
-        "Kazuo Ishiguro",
-        "James Joyce",
-        "Jonathan Kellerman",
-        "Wally Lamb",
-        "Harper Lee",
-        "Jack London",
-        "Bernard Malamud",
-        "Gabriel García Márquez",
-        "Anne McCaffrey",
-        "Margaret Mitchell",
-        "Toni Morrison",
-        "Sidney Sheldon",
-        "Danielle Steel",
-        "J. R. R. Tolkien",
-        "Alice Walker",
-        "Vladimir Nabokov",
-        "Joyce Carol Oates",
-        "Michael Ondaatje",
-        "George Orwell",
-        "James Patterson",
-        "Thomas Pynchon",
-        "Ayn Rand",
-        "Salmon Rushdie",
-        "J. D. Salinger",
-        "Jane Smiley",
-        "Paul Theroux",
-        "Kurt Vonnegut",
-        "E. B. White",
-        "Thomas Wolfe",
-        "Virginia Woolf",
-        "Herman Wouk"
+    {"Sidney Sheldon", 1},
+    {"Danielle Steele", 1},
+    {"Jackie Collins", 1},
+    {"Judith Krantz", 1},
+    {"Nora Roberts", 1},
+    {"Iris Johansen", 1},
+    {"Diana Palmer", 1},
+    {"Catherine Anderson", 1},
+    {"Joy Fielding", 1},
+    {"Nicholas Sparks", 1},
+    {"Stephen Hawking", 1},
+    {"Stephen J. Gould", 1},
+    {"Richard Dawkins", 1},
+    {"Thomas Kuhn", 1},
+    {"Ernst Mayr", 1},
+    {"Douglas Rushkoff", 1},
+    {"Amir D. Aczel", 1},
+    {"Robert Jordan", 1},
+    {"Douglas Adams", 1},
+    {"Anne McCaVrey", 1},
+    {"William Gibson", 1},
+    {"Terry Brooks", 1},
+    {"Terry Goodkind", 1},
+    {"Piers Anthony", 1},
+    {"Arthur C. Clarke", 1},
+    {"Ray Bradbury", 1},
+    {"Ursula K. Le Guin", 1},
+    {"Roland Barthes", 1},
+    {"Jean Baudrillard", 1},
+    {"Michel Foucault", 1},
+    {"Bertrand Russell", 1},
+    {"Antonio Damasio", 1},
+    {"Daniel Goleman", 1},
+    {"Dean Koontz", 1},
+    {"John LeCarré", 1},
+    {"Robert Ludlum", 1},
+    {"Clive Cussler", 1},
+    {"Sue Grafton", 1},
+    {"Ian Rankin", 1},
+    {"P. D. James", 1},
+    {"John Saul", 1},
+    {"Patricia Cornwell", 1},
+    {"Ken Follett", 1},
+    {"Noam Chomsky", 1},
+    {"Norman Mailer", 1},
+    {"Michael Moore", 1},
+    {"Eric Schlosser", 1},
+    {"Bob Woodward", 1},
+    {"Pierre Berton", 1},
+    {"Naomi Klein", 1},
+    {"John Updike", 1},
+    {"W. O. Mitchell", 1},
+    {"Alice Munro", 1},
+    {"Maeve Binchy", 1},
+    {"Carol Shields", 1},
+    {"John Irving", 1},
+    {"Toni Morrison", 1},
+    {"Amy Tan", 1},
+    {"Rohinton Mistry", 1},
+    {"Sinclair Ross", 1},
+    {"Jack Canfield", 1},
+    {"Philip C. McGraw", 1},
+    {"M. Scott Peck", 1},
+    {"Robert Fulghum", 1},
+    {"Erma Bombeck", 1},
+    {"Jean Vanier", 1},
+    {"Stephen R. Covey", 1},
+    {"José Saramago", 1},
+    {"Yukio Mishima", 1},
+    {"Gabriel Garcia Marquez", 1},
+    {"Albert Camus", 1},
+    {"Umberto Eco", 1},
+    {"Milan Kundera", 1},
+    {"Paulo Coelho", 1},
+    {"W. G. Sebald", 1},
+    {"Italo Calvino", 1},
+    {"Thomas Mann", 1},
+    {"Faith Popcorn", 1},
+    {"Jim Collins", 1},
+    {"Napoleon Hill", 1},
+    {"Robert T. Kiyosaki", 1},
+    {"Stephen C. Lundin", 1},
+    {"Peter S. Pande", 1},
+    {"Kenneth H. Blanchard", 1},
+    {"Matt Ridley", 1},
+    {"John Maynard Smith", 1},
+    {"Diane Ackerman", 1},
+    {"JeVrey Gray", 1},
+    {"Joseph LeDoux", 1},
+    {"Oliver Sacks", 1},
+    {"Naomi Wolf", 1},
+    {"Robert D. Kaplan", 1},
+    {"Susan Sontag", 1},
+    {"Melody Beattie", 1},
+    {"Deepak Chopra", 1},
+    {"Marianne Williamson", 1},
+    {"Peter F. Drucker", 1},
+    {"Barry Z. Posner", 1},
+    {"M. D. Johnson Spencer", 1},
+    {"Lauren Adamson", -1}, #foils start here
+    {"Eric Amsel", -1},
+    {"Margaritia Azmitia", -1},
+    {"Oscar Barbarin", -1},
+    {"Reuben Baron", -1},
+    {"Gary Beauchamp", -1},
+    {"Thomas Bever", -1},
+    {"Elliot Blass", -1},
+    {"Dale Blyth", -1},
+    {"Hilda Borko", -1},
+    {"John Condry", -1},
+    {"Edward Cornell", -1},
+    {"Carl Corter", -1},
+    {"Diane Cuneo", -1},
+    {"Denise Daniels", -1},
+    {"Geraldine Dawson", -1},
+    {"Aimee Dorr", -1},
+    {"W Patrick Dickson", -1},
+    {"Robert Emery", -1},
+    {"Frances Fincham", -1},
+    {"Martin Ford", -1},
+    {"Harold Gardin", -1},
+    {"Frank Gresham", -1},
+    {"Robert Inness", -1},
+    {"Frank Keil", -1},
+    {"Reed Larson", -1},
+    {"Lynn Liben", -1},
+    {"Hugh Lytton", -1},
+    {"Franklin Manis", -1},
+    {"Morton Mendelson", -1},
+    {"James Morgan", -1},
+    {"Scott Paris", -1},
+    {"Richard Passman", -1},
+    {"David Perry", -1},
+    {"Miriam Sexton", -1},
+    {"K Warner Schaie", -1},
+    {"Robert Siegler", -1},
+    {"Mark Strauss", -1},
+    {"Alister Younger", -1},
+    {"Steve Yussen", -1}
 ]
 
-# table = "<table><p><b>Vocabulary</b></p>"
-# table += "<p style='text-align:left'>In the test below, you are given a word in capital letters. Select the word that means the same thing, or most nearly the same thing, as the capitalized word.</p>"
-# table += "<p style='text-align:left'>If you don't know, guess.</p>"
-#
-# q_number = 1
-#
-# for list in words:
-#     answers = []
-#     table += "<tr><td>"+ str(q_number) + ". " + list[0] + ":</td><td>"
-#     correct = "<input name='vocab_" + str(q_number) +"' type='radio' value='1' class='obligatory' required='required' id='vocab_" + str(q_number) + "_1' />"
-#     correct += "<label for='vocab_" + str(q_number) + "_1'>" + list[1] + "</label><br />"
-#     answers.append(correct)
-#     incorrect_1 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
-#     incorrect_1 += "<label for='vocab_" + str(q_number) + "_0'>" + list[2] + "</label><br />"
-#     answers.append(incorrect_1)
-#     incorrect_2 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
-#     incorrect_2 += "<label for='vocab_" + str(q_number) + "_0'>" + list[3] + "</label><br />"
-#     answers.append(incorrect_2)
-#     incorrect_3 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
-#     incorrect_3 += "<label for='vocab_" + str(q_number) + "_0'>" + list[4] + "</label><br />"
-#     answers.append(incorrect_3)
-#     random.shuffle(answers)
-#     for item in answers:
-#         table += item
-#     table += "<br />"
-#     q_number +=1
+table = "<table><p><b>Authors</b></p>"
+table += "<p style='text-align:left'>The following list of names contains some authors and non-authors. Select all the names of authors you know (that is, anyone who has written at least one book).</p>"
+table += "<p style='text-align:left'>If you don't know, do not guess. You will lose a point for every name you select who is not an author.</p>"
+
+q_number = 1
+answers = []
+
+for name, number in authors:
+    table += "<tr><td>"+ str(q_number) + ". " + list[0] + ":</td><td>"
+    answers+ = "<input id='ART" + name + str(number) + "' name='" + name + str(number) + "' type='checkbox' value='yes' /><tr>" + name + "</tr>"
+
+random.shuffle(answers)
+for item in answers:
+    table += item
+
+table += "<br />"
+q_number +=1
 
 table += "</table>"
 
---
+++ vocab/createhtml3.py
@@ -0,0 +1,115 @@
+import random
+import sys
+
+#source: Words that Go Together, Dabrowska 2015
+
+words = [
+    ["blatant lie", "clear lie", "conspicuous lie", "distinct lie", "recognizable lie"],
+    ["blank expression", "frightful expression", "frightful expression", "plain expression", "sinster expression", "terrible expression"],
+    ["attact publicity", "bring publicity", "make publicity", "win publicity", "attain publicity"],
+    ["fair share", "honest share", "just share", "legitimate share", "reasonable share"],
+    ["arouse suspicions", "incite suspicions", "kindle suspicions", "revive suspicions", "stimulate suspicions"],
+    ["raise prices","elevate prices", "grow prices", "lift prices", "stimulate prices"],
+    ["hazard a guess", "chance a guess", "dare a guess", "gamble a guess", "risk a guess"],
+    ["bend rules", "honour rules", "institute rules", "reject rules", "validate rules"],
+    ["issue a statement", "believe a statement", "change a statement", "offer a statement", "revise a statement"],
+    ["raise standards", "advance standards", "boost standards", "elevate standards", "lift standards"],
+    ["boost production", "double production", "enlarge production", "extend production", "redouble production"],
+    ["join the ranks", "combine the ranks", "conjoin the ranks", "merge the ranks", "unify the ranks"],
+    ["bitter dispute", "cruel dispute", "hard dispute", "harsh dispute", "savage dispute"],
+    ["absolute silence", "pure silence", "sheer silence", "stark silence", "supreme silence"],
+    ["full confession", "complete confession", "exhaustive confession", "extensive confession", "thorough confession"],
+    ["gain popularity", "acquire popularity", "attract popularity", "earn popularity", "get popularity"],
+    ["regular employment",  "constant employment", "normal employment", "ordinary employment", "unbroken employment"],
+    ["witness an incident", "glimpse an incident", "notice an incident", "observe an incident", "see an incident"],
+    ["achieve one's objectives", "complete one's objectives", "finish one's objectives", "follow one's objectives", "tackle one's objectives"],
+    ["general direction", "accurate direction", "appropriate direction", "convenient direction", "specific direction"],
+    ["divert attention", "apply attention", "dedicate attention", "grasp attention", "sidetrack attention"],
+    ["serious problem", "extensive problem", "extreme problem", "significant problem", "vital problem"],
+    ["urgent matters", "compelling matters", "critical matters",  "desperate matters", "major matters"],
+    ["close similarity", "doubtful similarity",  "evident similarity", "evident similarity", "extreme similarity", "near similarity"],
+    ["hear rumours", "contradict rumours", "discover rumours", "know rumours", "tell rumours"],
+    ["memorable phrase", "effective phrase", "helpful phrase", "noteworthy phrase", "significant phrase"],
+    ["divert suspicion", "distract suspicion", "mislead suspicion", "redirect suspicion", "sidetrack suspicion"],
+    ["restore faith", "instil faith", "bring faith", "offer faith", "refresh faith"],
+    ["thorough search", "complete search", "full search", "scrupulous search", "total search"],
+    ["precise details", "abundant details", "complete details",  "definite details", "small details"],
+    ["inflict punishment",  "apply punishment", "deliver punishment", "perform punishment", "provide punishment"],
+    ["attractive proposition", "appealing proposition", "charming proposition", "inviting proposition", "seductive proposition"],
+    ["dim view", "dark view", "murky view",  "shadowy view", "shady view"],
+    ["outspoken critic",  "aggressive critic",  "forthright critic", "frank critic", "open critic"],
+    ["odd remark", "peculiar remark", "queer remark", "unnatural remark", "weird remark"],
+    ["striking example", "distinct example", "gross example", "recognizable example", "shocking example"],
+    ["lodge a complaint", "formulate a complaint", "place a complaint",  "record a complaint", "write a complaint"],
+    ["obvious conclusion", "confident conclusion", "evident conclusion", "solid conclusion", "sure conclusion"],
+    ["overall responsibility", "general responsibility", "large responsibility", "single responsibility", "unique responsibility"],
+    ["refuse an application", "decline an application", "deny an application", "ignore an application", "scrap an application"]
+]
+
+table = "<table><p><b>Words that Go Together</b></p>"
+table += "<p style='text-align:left'>This questionnaire consists of sets of five phrases. From each set, <b>choose one phrase that sounds the most natural or familiar. If you are not sure, guess. </b>Here are two examples:.</p>"
+table += "<p style='text-align:left'>delicate tea <br>feeble tea <br>frail tea <br> powerless tea <br><b> weak tea</b></p>"
+table += "<p style='text-align:left'><b>deliver a speech</b> <br>hold a speech <br>perform a speech <br>present a speech <br>utter a speech</p>"
+table += "<p style='text-align:left'>The words delicate, feeble, frail, powerless and weak are similar in meaning; but with tea, we would normally use <b>weak</b>. In the second example, <b>deliver</b> a speech sounds more natural than the other choices.</p>"
+
+q_number = 1
+
+for list in words:
+    answers = []
+    table += "<tr><td>"+ str(q_number) + ". </td><td>"
+    correct = "<input name='vocab_" + str(q_number) +"' type='radio' value='1' class='obligatory' required='required' id='vocab_" + str(q_number) + "_1' />"
+    correct += "<label for='vocab_" + str(q_number) + "_1'>" + list[0] + "</label><br />"
+    answers.append(correct)
+    incorrect_1 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+    incorrect_1 += "<label for='vocab_" + str(q_number) + "_0'>" + list[1] + "</label><br />"
+    answers.append(incorrect_1)
+    incorrect_2 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+    incorrect_2 += "<label for='vocab_" + str(q_number) + "_0'>" + list[2] + "</label><br />"
+    answers.append(incorrect_2)
+    incorrect_3 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+    incorrect_3 += "<label for='vocab_" + str(q_number) + "_0'>" + list[3] + "</label><br />"
+    answers.append(incorrect_3)
+    incorrect_4 = "<input name='vocab_" + str(q_number) +"' type='radio' value='0' class='obligatory' required='required' id='vocab_" + str(q_number) + "_0' />"
+    incorrect_4 += "<label for='vocab_" + str(q_number) + "_0'>" + list[4] + "</label><br />"
+    answers.append(incorrect_4)
+    random.shuffle(answers)
+    for item in answers:
+        table += item
+    table += "<br />"
+    q_number +=1
+
+table += "</table>"
+
+html = """
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+	<link rel="stylesheet" type="text/css" href="https://www.psycholinguistics.ml/css/global_main.css">
+	<link rel="stylesheet" type="text/css" href="https://www.psycholinguistics.ml/css/global_mainB.css">
+	<link rel="stylesheet" type="text/css" href="https://www.psycholinguistics.ml/css/Form.css">
+</head>
+<body>
+<form id="mainform" style="text-align: center" method="post" action="https://www.psycholinguistics.ml/jspsych_5/write_data.php">
+
+"""
+
+html += table
+
+html += """
+
+<input name="submit" type="submit" value="Submit"/>
+</form>
+</body>
+</html>
+"""
+
+print(html)
+
+# with open("index.html", "w+") as out:
+# 	out.write(html)
+#
+# if "-m" not in sys.argv:
+# 	print(html)
+#
+# print("Done.")
