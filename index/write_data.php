7fa2b3517fc7bf2c9f6a7837545f6fb8aec62489
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2019-12-04 10:31:41 +0100

    redirect index

--- index/write_data.php
+++ index/write_data.php
@@ -82,32 +82,12 @@ $query = "UPDATE participants
 		reading_enjoyment='".$reading_enj."'
 		WHERE prolific_id='".$prolificID."'";
 
-if (!$conn-> query($query)) {
-    //echo '<p style="text-align:center; font-family: Lucida, Console, monospace; font-size: medium;">Failed. Have you already done the experiment?</p>';
-    echo "Execute failed: (" . $conn->errno . ") " . $conn->error;
-} else {
-	switch (substr($testgroup, 0, 1)){
+    if (!$conn->query($query)) {
+        die("Execute failed");
+    }
 
-		case "1":
-			setcookie("ibex_1_group", $ibex_1_group, time()+144000, "/", "psycholinguistics.ml");
-			$next = "https://www.psycholinguistics.ml/ibex_1/experiment.html";
-			break;
+    $next ="https://www.psycholinguistics.ml/index/info.html";
 
-		case "2":
-			$next = "https://www.psycholinguistics.ml/ibex_2/experiment.html";
-			break;
-
-		case "J":
-			setcookie("jspsych_group", $jspsych_group, time()+144000, "/", "psycholinguistics.ml");
-			setcookie("jspsych_progress", $jspsych_progress, time()+144000, "/", "psycholinguistics.ml");
-			$next = "https://www.psycholinguistics.ml/jspsych.html";
-			break;
-		default:
-			$next = "https://www.psycholinguistics.ml/index/server_error.html";
-			break;
-	}
-
-    echo "Redirecting...<br /> If nothing happens, <a href='" . $next ."'>click here</a>";
 	$conn->commit();
 	$conn->close();
 	header("Location: ". $next, true, 302);
