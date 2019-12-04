90352423ffce2410827a7b06f77b87c1ae22d08d
Jiri Zamecnik <jerry.zamecnik@gmail.com>
2019-12-04 11:54:19 +0100

    Move vocab 1 to JSPsych

--- vocab/write_data_timed.php
+++ vocab/write_data_timed.php
@@ -2,13 +2,13 @@
 ob_start();
 $post_data = json_decode(file_get_contents('php://input'), true); 
 // the directory "data" must be writable by the server
-$name = "data_4/".$post_data['filename'].".csv"; 
+$name = "data/".$post_data['filename'].".csv"; 
 $data = $post_data['filedata'];
 // write the file to disk
 file_put_contents($name, $data);
 
 header('Content-Type: application/json');
-echo json_encode(['location'=>'https://www.psycholinguistics.ml/get_next_jspsych.php']);
+echo json_encode(['location'=>'https://www.psycholinguistics.ml/vocab/index2.html']);
 ob_end_flush();
 exit();
 ?>
