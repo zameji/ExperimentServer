09bbbc72d604660dec7c6f409ceb4824f86993e6
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2019-12-10 16:05:59 +0100

    replaced Navon with shaped-based Navon, also reduced 96 to 50 trials

--- archive_jspsych_Navon/experiment.js
+++ archive_jspsych_Navon/experiment.js
@@ -16,6 +16,51 @@ function evalAttentionChecks() {
   return check_percent
 }
 
+function assessPerformance() {
+	/* Function to calculate the "credit_var", which is a boolean used to
+	credit individual experiments in expfactory. */
+	var experiment_data = jsPsych.data.getTrialsOfType('poldrack-single-stim')
+	experiment_data = experiment_data.concat(jsPsych.data.getTrialsOfType('poldrack-categorize'))
+	var missed_count = 0
+	var trial_count = 0
+	var rt_array = []
+	var rt = 0
+	//record choices participants made
+	var choice_counts = {}
+	choice_counts[-1] = 0
+	for (var k = 0; k < choices.length; k++) {
+		choice_counts[choices[k]] = 0
+	}
+	for (var i = 0; i < experiment_data.length; i++) {
+		if (experiment_data[i].possible_responses != 'none') {
+			trial_count += 1
+			rt = experiment_data[i].rt
+			key = experiment_data[i].key_press
+			choice_counts[key] += 1
+			if (rt == -1) {
+				missed_count += 1
+			} else {
+				rt_array.push(rt)
+			}
+		}
+	}
+	//calculate average rt
+	var avg_rt = -1
+	if (rt_array.length !== 0) {
+		avg_rt = math.median(rt_array)
+	}
+	//calculate whether response distribution is okay
+	var responses_ok = true
+	Object.keys(choice_counts).forEach(function(key, index) {
+		if (choice_counts[key] > trial_count * 0.85) {
+			responses_ok = false
+		}
+	})
+	var missed_percent = missed_count/trial_count
+	credit_var = (missed_percent < 0.4 && avg_rt > 200 && responses_ok)
+	jsPsych.data.addDataToLastTrial({"credit_var": credit_var})
+}
+
 var randomDraw = function(lst) {
   var index = Math.floor(Math.random() * (lst.length))
   return lst[index]
@@ -23,7 +68,7 @@ var randomDraw = function(lst) {
 
 var makeTrialList = function(len, stim, data) {
   //choice array: numeric key codes for the numbers 1-4
-  var choice_array = choices
+  var choice_array = [83, 72]
     // 1 is a switch trial: ensure half the trials are switch trials
   var switch_trials = jsPsych.randomization.repeat([0, 1], len / 2)
     //create test array
@@ -39,11 +84,11 @@ var makeTrialList = function(len, stim, data) {
   tmp_obj.stimulus = stim[tmpi]
   var tmp_data = $.extend({}, data[tmpi])
   tmp_data.switch = 0
-  tmp_data.correct_response = choice_array[task_shapes.indexOf(data[tmpi][trial_index + '_shape'])]
+  tmp_data.correct_response = choice_array[local_shapes.indexOf(data[tmpi][trial_index + '_shape'])]
   tmp_obj.data = tmp_data
   output_list.push(tmp_obj)
     /* randomly sample from either the global or local stimulus lists (first and half part of the stim/data arrays)
-  On stay trials randomly select an additional stimulus from that array. On switch trials choose from the other list. */
+	On stay trials randomly select an additional stimulus from that array. On switch trials choose from the other list. */
   for (i = 1; i < switch_trials.length; i++) {
     tmp_obj = {}
     if (switch_trials[i] == 1) {
@@ -61,7 +106,7 @@ var makeTrialList = function(len, stim, data) {
     tmp_obj.stimulus = stim[tmpi]
     tmp_data = $.extend({}, data[tmpi])
     tmp_data.switch = switch_trials[i]
-    tmp_data.correct_response = choice_array[task_shapes.indexOf(data[tmpi][trial_index +
+    tmp_data.correct_response = choice_array[local_shapes.indexOf(data[tmpi][trial_index +
       '_shape'
     ])]
     tmp_obj.data = tmp_data
@@ -84,13 +129,15 @@ var run_attention_checks = false
 var attention_check_thresh = 0.65
 var sumInstructTime = 0 //ms
 var instructTimeThresh = 0 ///in seconds
+var credit_var = true
 
 // task specific variables
-var choices = [49, 50, 51, 52]
 var current_trial = 0
+var choices = [72, 83]
 var task_colors = jsPsych.randomization.shuffle(['blue', 'black'])
-var task_shapes = ['circle', 'X', 'triangle', 'square']
-var path = 'images/'
+var global_shapes = ['s','h', 'o']
+var local_shapes = ['s','h','o']
+var path = '/static/experiments/local_global_letter/images/'
 var prefix = '<div class = centerbox><img src = "'
 var postfix = '"</img></div>'
 var stim = []
@@ -99,33 +146,35 @@ var images = []
 for (c = 0; c < task_colors.length; c++) {
   if (c === 0) {
     condition = 'global'
+    global_shape_length = 2
+    local_shape_length = 3
   } else {
     condition = 'local'
+    global_shape_length = 3
+    local_shape_length = 2
   }
-  for (g = 0; g < task_shapes.length; g++) {
-    for (l = 0; l < task_shapes.length; l++) {
-      images.push([path + task_colors[c] + '_' + task_shapes[g] + 'of' + task_shapes[l] +
-        's.png'])
-      stim.push(prefix + path + task_colors[c] + '_' + task_shapes[g] + 'of' + task_shapes[l] +
-        's.png' + postfix)
+  for (g = 0; g < global_shape_length; g++) {
+    for (l = 0; l < local_shape_length; l++) {
+      stim.push(prefix + path + task_colors[c] + '_' + global_shapes[g] + '_of_' + local_shapes[l] +
+        '.png' + postfix)
+      images.push(path + task_colors[c] + '_' + global_shapes[g] + '_of_' + local_shapes[l] +
+        '.png')
       data.push({
         condition: condition,
-        global_shape: task_shapes[g],
-        local_shape: task_shapes[l]
+        global_shape: global_shapes[g],
+        local_shape: local_shapes[l]
       })
     }
   }
 }
 
-//preload images
 jsPsych.pluginAPI.preloadImages(images)
-
 //Set up experiment stimulus order
-var practice_trials = makeTrialList(36, stim, data)
+var practice_trials = makeTrialList(5, stim, data)  //36
 for (i = 0; i < practice_trials.length; i++) {
   practice_trials[i].key_answer = practice_trials[i].data.correct_response
 }
-var test_trials = makeTrialList(50, stim, data)
+var test_trials = makeTrialList(50, stim, data) //96
 
 
 
@@ -154,6 +203,7 @@ var attention_node = {
 var post_task_block = {
    type: 'survey-text',
    data: {
+       exp_id: "local_global_letter",
        trial_id: "post task questions"
    },
    questions: ['<p class = center-block-text style = "font-size: 20px">Please summarize what you were asked to do in this task.</p>',
@@ -167,16 +217,17 @@ var end_block = {
   type: 'poldrack-text',
   data: {
     trial_id: "end",
-    exp_id: 'local_global_shape'
+    exp_id: 'local_global_letter'
   },
   timing_response: 180000,
   text: '<div class = centerbox><p class = center-block-text>Thanks for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
   cont_key: [13],
-  timing_post_trial: 0
+  timing_post_trial: 0,
+  on_finish: assessPerformance
 };
 
 var feedback_instruct_text =
-  'Welcome to the experiment. This experiment will take about 8 minutes. Press <strong>enter</strong> to begin.'
+  'Welcome to the experiment. This experiment will take about 5 minutes. Press <strong>enter</strong> to begin.'
 var feedback_instruct_block = {
   type: 'poldrack-text',
   cont_key: [13],
@@ -194,22 +245,23 @@ var instructions_block = {
     trial_id: "instruction"
   },
   pages: [
-    '<div class = centerbox><p class = block-text>In this experiment you will see blue or black shapes made up of smaller shapes, like the image below. All of the smaller shapes will always be the same shape. Both the large shape and the smaller shapes can either be a circle, X, triangle or square.</p><div class = instructionImgBox><img src = "images/blue_squareofcircles.png" height = 200 width = 200></img></div></div>',
-    '<div class = centerbox><p class = block-text>Your task is to respond based on how many lines either the large or small shapes have, depending on the color. If the shape is ' +
-    task_colors[0] + ' respond based on how many lines the large shape has. If the shape is ' +
+    '<div class = centerbox><p class = block-text>In this experiment you will see blue or black letters made up of smaller letters, like the image below. All of the smaller letters will always be the same letter.</p><div class = instructionImgBox><img src = "/static/experiments/local_global_letter/images/blue_s_of_o.png" height = 200 width = 200></img></div></div>',
+    '<div class = centerbox><p class = block-text>Your task is to indicate whether the larger or smaller letters is an "H" or "S", depending on the color. If the letter is ' +
+    task_colors[0] + ' indicate whether the larger letter is an "H" or "S". If the letter is ' +
     task_colors[1] +
-    ' respond based on how many lines the small shape has.</p><p class = block-text>Use the number keys to respond 1 for a circle, 2 for an X, 3 for a triangle and 4 for a square.</p></div>',
-    '<div class = centerbox><p class = block-text>For instance, for the shape below you would press 3 because it is ' +
+    ' indicate whether the smaller letter is an "H" or "S".</p><p class = block-text>Use the "H" or "S" keys to indicate the letter.</p></div>',
+    '<div class = centerbox><p class = block-text>For instance, for the letter below you would press "S" because it is ' +
     task_colors[1] +
     ' which means you should respond based on the smaller shapes. If the shape was instead ' +
     task_colors[0] +
-    ' you would press 2.</p><div class = instructionImgBox><img src = "images/' +
-    task_colors[1] + '_Xoftriangles.png" height = 200 width = 200></img></div></div>'
+    ' you would press "H".</p><div class = instructionImgBox><img src = "/static/experiments/local_global_letter/images/' +
+    task_colors[1] + '_h_of_s.png" height = 200 width = 200></img></div></div>'
   ],
   allow_keys: false,
   show_clickable_nav: true,
   timing_post_trial: 1000
 };
+
 
 var instruction_node = {
   timeline: [feedback_instruct_block, instructions_block],
@@ -250,10 +302,10 @@ var start_test_block = {
   data: {
     trial_id: "test_intro"
   },
-  text: '<div class = centerbox><p class = center-block-text>We will now start the test. Remember, if the shape is ' +
-    task_colors[0] + ' respond based on how many lines the large shape has. If the shape is ' +
+  text: '<div class = centerbox><p class = center-block-text>We will now start the test. Remember, if the letter is ' +
+    task_colors[0] + ' indicate whether the larger letter is an "H" or "S". If the letter is ' +
     task_colors[1] +
-    ' respond based on how many lines the small shape has.</p><p class = center-block-text>Press <strong>enter</strong> to begin.</p></div>',
+    ' indicate whether the smaller letter is an "H" or "S".</p><p class = center-block-text>Press <strong>enter</strong> to begin.</p></div>',
   cont_key: [13],
   timing_post_trial: 1000,
   on_finish: function() {
@@ -299,8 +351,8 @@ var test_block = {
   timing_post_trial: 500,
   timing_response: 2000,
   on_finish: function(data) {
-    correct = false
-  	if (data.key_press === data.correct_response) {
+  	correct = false
+    if (data.key_press === data.correct_response) {
       correct = true
     }
   	jsPsych.data.addDataToLastTrial({
@@ -312,12 +364,12 @@ var test_block = {
 };
 
 /* create experiment definition array */
-var local_global_shape_experiment = [];
-local_global_shape_experiment.push(instruction_node);
-local_global_shape_experiment.push(start_practice_block);
-local_global_shape_experiment.push(practice_block);
-local_global_shape_experiment.push(start_test_block);
-local_global_shape_experiment.push(test_block);
-local_global_shape_experiment.push(attention_node)
-local_global_shape_experiment.push(post_task_block)
-local_global_shape_experiment.push(end_block);
+var local_global_letter_experiment = [];
+local_global_letter_experiment.push(instruction_node);
+local_global_letter_experiment.push(start_practice_block);
+local_global_letter_experiment.push(practice_block);
+local_global_letter_experiment.push(start_test_block);
+local_global_letter_experiment.push(test_block);
+local_global_letter_experiment.push(attention_node)
+local_global_letter_experiment.push(post_task_block)
+local_global_letter_experiment.push(end_block);
---
+++ jspsych_Navon/README.md
@@ -0,0 +1,13 @@
+# Experiment Factory Experiment
+
+Hi Friend! This is an Experiment that is friendly for use in the [Experiment Factory](https://expfactory.github.io/expfactory). You can run it locally by putting these files in a web server, or use the Experiment Factory to generate a reproducible container. Check out the documentation above for more information, or [post an issue](https://www.github.com/expfactory/expfactory/issues) if you have any questions.
+
+![https://expfactory.github.io/expfactory/img/expfactoryticketyellow.png](https://expfactory.github.io/expfactory/img/expfactoryticketyellow.png)
+
+## Containers and Metadata
+The repository builds and provides a set of containers and metadata files to help you run and understand the experiment.
+
+ - [preview the experiment](https://expfactory-experiments.github.io/local-global-shape/) on Github pages
+ - [use the container](https://hub.docker.com/r/expfactory/local-global-shape/) provided on Docker Hub, built from this repository. All previous versions are also found here based on Github commits.
+ - [inspect metadata](https://github.com/expfactory-experiments/local-global-shape/tree/gh-pages) including manifests and packages associated with Github commits. While this table isn't rendered on Github pages, if you render the branch and look at the index.html you will have a pretty rendering of it. 
+
