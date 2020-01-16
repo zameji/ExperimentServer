94521f1902b0a542b30a23eb9009037731167327
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-01-16 16:42:16 +0100

    debugging: try to remove the option for multiple tries of the same task

--- jspsych/write_data.php
+++ jspsych/write_data.php
@@ -1,16 +1,16 @@
 <?php
 ob_start();
-$post_data = json_decode(file_get_contents('php://input'), true); 
+$post_data = json_decode(file_get_contents('php://input'), true);
 // the directory "data" must be writable by the server
-$name = "data/".$post_data['filename'].".csv"; 
+$name = "data/".$post_data['filename'].".csv";
 $data = $post_data['filedata'];
-// write the file to disk
-if (!file_exists($name)){
-	file_put_contents($name, $data);
-} else {
-	$name = 'data/r'.substr($name, 5, strlen($name)-9,).'_'.date('z-G-i').".csv";
-	file_put_contents($name, $data);
-}
+// // write the file to disk
+// if (!file_exists($name)){
+// 	file_put_contents($name, $data);
+// } else {
+// 	$name = 'data/r'.substr($name, 5, strlen($name)-9,).'_'.date('z-G-i').".csv";
+// 	file_put_contents($name, $data);
+// }
 
 header('Content-Type: application/json');
 echo json_encode(['location'=>'https://www.psycholinguistics.ml/get_next_jspsych.php']);
