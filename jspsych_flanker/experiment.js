8148d4ba6d27678d6367243637b6c724a0748513
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-01-17 11:12:13 +0100

    flannker took out long pause before feedback

--- jspsych_flanker/experiment.js
+++ jspsych_flanker/experiment.js
@@ -242,7 +242,6 @@ for (i = 0; i < practice_len; i++) {
 		timing_feedback_duration: 1000,
 		show_stim_with_feedback: false,
 		timing_response: 1500,
-		timing_post_trial: 500,
 		on_finish: function() {
 			jsPsych.data.addDataToLastTrial({
 				exp_stage: "practice"
@@ -273,7 +272,6 @@ for (i = 0; i < exp_len; i++) {
 		timing_feedback_duration: 1000,
 		timing_response: 2000,
 		show_stim_with_feedback: false,
-		timing_post_trial: 100,
 		on_finish: function() {
 			jsPsych.data.addDataToLastTrial({
 				exp_stage: "test"
