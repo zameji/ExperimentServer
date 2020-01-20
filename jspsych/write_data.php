bbdfb66c3ae4c15901c0828512376d585ba7da91
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-01-20 09:57:29 +0100

    change php to always include date in file (needs to be tested)

--- jspsych/write_data.php
+++ jspsych/write_data.php
@@ -2,15 +2,21 @@
 ob_start();
 $post_data = json_decode(file_get_contents('php://input'), true);
 // the directory "data" must be writable by the server
-$name = "data/".$post_data['filename'].".csv";
+
+$name = "data/".$post_data['filename'].date('z-G-i').".csv";
 $data = $post_data['filedata'];
-// write the file to disk
-if (!file_exists($name)){
-	file_put_contents($name, $data);
-} else {
-	$name = 'data/r'.substr($name, 5, strlen($name)-9,).'_'.date('z-G-i').".csv";
-	file_put_contents($name, $data);
-}
+
+file_put_contents($name, $data);
+
+// $name = "data/".$post_data['filename'].".csv";
+// $data = $post_data['filedata'];
+// // write the file to disk
+// if (!file_exists($name)){
+// 	file_put_contents($name, $data);
+// } else {
+// 	$name = 'data/r'.substr($name, 5, strlen($name)-9,).'_'.date('z-G-i').".csv";
+// 	file_put_contents($name, $data);
+// }
 
 header('Content-Type: application/json');
 echo json_encode(['location'=>'https://www.psycholinguistics.ml/get_next_jspsych.php']);
