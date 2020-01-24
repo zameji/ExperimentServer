0bfcc18a34abdc127f6bfedba5f99696a961329a
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-01-24 12:38:04 +0100

    change php for vocab tasks (to add date)

--- vocab/write_data4_timed.php
+++ vocab/write_data4_timed.php
@@ -2,15 +2,21 @@
 ob_start();
 $post_data = json_decode(file_get_contents('php://input'), true);
 // the directory "data" must be writable by the server
-$name = "data_4/".$post_data['filename'].".csv";
+
+$name = "data/".$post_data['filename']."_".date('m-z-G-i').".csv";
 $data = $post_data['filedata'];
-// write the file to disk
-if (!file_exists($name)){
-	file_put_contents($name, $data);
-} else {
-	$name = 'data_4/r'.substr($name, 7, strlen($name)-11,).'_'.date('z-G-i').".csv";
-	file_put_contents($name, $data);
-}
+
+file_put_contents($name, $data);
+
+// $name = "data_4/".$post_data['filename'].".csv";
+// $data = $post_data['filedata'];
+// // write the file to disk
+// if (!file_exists($name)){
+// 	file_put_contents($name, $data);
+// } else {
+// 	$name = 'data_4/r'.substr($name, 7, strlen($name)-11,).'_'.date('z-G-i').".csv";
+// 	file_put_contents($name, $data);
+// }
 
 header('Content-Type: application/json');
 echo json_encode(['location'=>'https://www.psycholinguistics.ml/vocab/index2_timed.html']);
