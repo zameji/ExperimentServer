6b2f1d6dfb0e48e98298fa08922e9285a7971016
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-02-03 18:46:02 +0100

    update instructions vocab

--- vocab/experiment_4.js
+++ vocab/experiment_4.js
@@ -133,7 +133,7 @@ var instructions_block = {
   type: 'poldrack-instructions',
   pages: [
     '<div class = centerbox><p class = block-text>In this task, you will be shown a CAPITALIZED WORD, together with an example of how it is used.</p>'+
-	'<p class = block-text>Your task is to find the meaning of the word among the options below the word and press the letter corresponding to the meaning.</p>'+
+	'<p class = block-text>Select the option that is CLOSEST to the meaning of the capitalized word.</p>'+
   '<p class = block-text>Important! Please be sure to keep all other browser windows closed while doing this task.</p>'+
   '<p class = block-text>Our algorithm is very sensitive and may flag your responses as being suspicious if other tabs, windows, or programs are open.</p>'+
   '<p class = block-text><b>You will have a set amount of time to answer each question.</b>'+
