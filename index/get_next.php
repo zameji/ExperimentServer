e4769f62a1ae22e4bc382c878923950022cc7fde
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-02-10 17:27:25 +0100

    add echos to see whats going on in the redirect

--- index/get_next.php
+++ index/get_next.php
@@ -62,7 +62,9 @@ if ($result->num_rows > 0) {
 				  setcookie("ibex_1_group", $ibex_1_group, time()+144000, "/", "psycholinguistics.ml");
 				  $next = "https://www.psycholinguistics.ml/ibex_1/experiment.html";
         } else {
-          $next = "https://www.psycholinguistics.ml/vocab/no_back.html";
+          #$next = "https://www.psycholinguistics.ml/vocab/no_back.html";
+          echo $comingFrom;
+          echo substr($testgroup, $previous_progress, 1);
         }
 				break;
 
@@ -80,7 +82,9 @@ if ($result->num_rows > 0) {
 				  setcookie("jspsych_progress", $jspsych_progress, time()+144000, "/", "psycholinguistics.ml");
           $next = "https://www.psycholinguistics.ml/jspsych.html";
         } else {
-          $next = "https://www.psycholinguistics.ml/vocab/no_back.html";
+          #$next = "https://www.psycholinguistics.ml/vocab/no_back.html";
+          echo $comingFrom;
+          echo substr($testgroup, $previous_progress, 1);
         }
 				break;
 
