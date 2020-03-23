78ba10eccedbbc81508e7902713583e24bdca665
Kyla McConnell <kylamcconnell@anglistik.uni-freiburg.de>
2020-03-23 11:14:04 +0100

    update instructions axcpt

--- jspsych_axcpt/experiment.js
+++ jspsych_axcpt/experiment.js
@@ -130,7 +130,7 @@ var instructions_block = {
   pages: [
     '<div class = centerbox><p class = block-text>In this task, you will see a red letter, followed by multiple black letters, and finally, another red letter. That forms one set. Then, a new set will start. (Total: about 7 minutes.)</p><p class = block-text>Your job is to respond by pressing the "J" key after ALL letters EXCEPT if the first red letter in the set was an "A" and the current letter is a RED "X".<b> If the first red letter was an "A" <strong>AND</strong> the current letter is a red "X", press the ' +
     possible_responses[0][0] + '. Otherwise press the ' + possible_responses[1][0] +
-    ' after every letter.</b> This means that you have to <b>press a key after every letter! </b></p><p class = block-text>Important: Do not press any keys before you see a letter. If you press a key before the letter is shown, you will lose a point for this section.</div>',
+    ' after every letter.</b> </p><p class = block-text>Important: Do not press any keys before you see a letter. If you press a key before the letter is shown, you will lose a point for this section.</div>',
     '<div class = centerbox><p class = block-text>You will now see a practice set. Press J after every letter, except the second red letter. At the second red letter, press F if the first red letter was an A and the current (red) letter is an X. Otherwise, press J again.</p></div>'
   ],
   allow_keys: false,
